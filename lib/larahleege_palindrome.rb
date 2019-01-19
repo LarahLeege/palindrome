3d33301830e5943b5a96f40d1ac9d726e46e1fef
larahleege <larahleege@gmx.de>
2019-01-19 10:41:34 +0100

    Initialize repository

---
+++ lib/larahleege_palindrome.rb
@@ -0,0 +1,6 @@
+require "larahleege_palindrome/version"
+
+module LarahleegePalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/larahleege_palindrome/version.rb
@@ -0,0 +1,3 @@
+module LarahleegePalindrome
+  VERSION = "0.1.0"
+end
